51e7d8e267ed9dd42127ccaf3b5602285b8e7cf2
Boldwin <boldwinbesa@gmail.com>
2021-10-06 16:03:38 +0200

    Initial Commits

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -12,7 +12,7 @@ database_url =
     """
 
 config :services, Services.Repo,
-  # ssl: true,
+  ssl: true,
   # url: database_url,
   # pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
   username: "postgres",
@@ -31,7 +31,7 @@ secret_key_base =
 
 config :services, ServicesWeb.Endpoint,
   http: [
-    port: String.to_integer(System.get_env("PORT") || "4000"),
+    port: String.to_integer(System.get_env("PORT") || "4010"),
     transport_options: [socket_opts: [:inet6]]
   ],
   secret_key_base: secret_key_base
