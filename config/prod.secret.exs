6eaaa5bd11dd85ae0b3c42534112da1ca2606cb2
Boldwin <boldwinbesa@gmail.com>
2021-10-06 16:17:01 +0200

    Initial Commits

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -36,12 +36,14 @@ config :services, ServicesWeb.Endpoint,
   ],
   secret_key_base: secret_key_base
 
+
 # ## Using releases (Elixir v1.9+)
 #
 # If you are doing OTP releases, you need to instruct Phoenix
 # to start each relevant endpoint:
 #
-#     config :services, ServicesWeb.Endpoint, server: true
+    config :services, ServicesWeb.Endpoint, server: true
+
 #
 # Then you can assemble a release by calling `mix release`.
 # See `mix help release` for more information.
